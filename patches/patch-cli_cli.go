ecda175b7713c6fef7258924bdc78ba2dfd3dba2
Amitai Schleier <schmonz-web-git@schmonz.com>
2023-03-15 15:18:22 -0400

    Add pkgsrc exercism to git for offline hacking.

---
+++ patches/patch-cli_cli.go
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Don't let the program try to update itself.
+
+--- cli/cli.go.orig	2022-10-04 09:26:56.000000000 +0000
++++ cli/cli.go
+@@ -95,6 +95,8 @@ func (c *CLI) IsUpToDate() (bool, error)
+ 
+ // Upgrade allows the user to upgrade to the latest version of the CLI.
+ func (c *CLI) Upgrade() error {
++	return fmt.Errorf("use pkgsrc tools to upgrade")
++
+ 	var (
+ 		OS   = osMap[runtime.GOOS]
+ 		ARCH = archMap[runtime.GOARCH]
